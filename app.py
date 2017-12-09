203bafd830f2c548626093aece2dbee0c8a945f3
jessexknight <jesse.x.knight@gmail.com>
2017-12-09 01:58:34 -0500

    major refactor: 1 file, 1 csv, dict repairs, add count

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 import os
+import re
+import csv
+import xlrd
+import dateparser
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -6,23 +11,199 @@ import plotly.graph_objs as go
 from dash.dependencies import Input, Output
 from flask import send_from_directory
 from collections import OrderedDict
-from ewhdata import *
-from config import *
-from utils import *
+
+# Config -----------------------------------------------------------------------
+
+lut = {}
+lut['year']      = os.path.join('static','list-year.csv')
+lut['fix']       = os.path.join('static','list-fix.csv')
+lut['result']    = os.path.join('static','list-result.csv')
+lut['country']   = os.path.join('static','list-country.csv')
+lut['equipment'] = os.path.join('static','list-equipment.csv')
+lut['table']     = os.path.join('static','def-table.csv')
+wsf = {}
+wsf['root']   = os.path.join('data')
+wsf['output'] = os.path.join('static','repairs.csv')
+idx = {}
+idx['sheet']     = 0
+idx['date']      = 'C6'
+idx['country']   = 'C7'
+idx['engineers'] = 'K6'
+idx['hospital']  = 'K7'
+idx['repair'] = {}
+idx['repair']['row0']   = '11'
+idx['repair']['equip']  = 'B'
+idx['repair']['oem']    = 'C'
+idx['repair']['model']  = 'D'
+idx['repair']['sn']     = 'E'
+idx['repair']['fix']    = 'FGHIJKL'
+idx['repair']['notes']  = 'M'
+idx['repair']['result'] = 'NO'
+sql = {}
+sql['connect'] = {}
+sql['connect']['user'] = 'ewh'
+sql['connect']['password'] = 'repairs'
+sql['connect']['host'] = '127.0.0.1'
+sql['database'] = 'repairs'
+sql['schema'] = {}
+sql['schema']['repair'] = os.path.join('repair-schema.csv')
+
+# Classes & Workers ------------------------------------------------------------
+
+class ExcelFile:
+  def __init__(self,filename,sheet):
+    self.filename = filename
+    self.sheet = sheet
+
+  def open(self):
+    if os.path.exists(self.filename):
+      return xlrd.open_workbook(filename=self.filename).sheet_by_index(self.sheet)
+
+  def col(self,colStr):
+    idxs = [match for match in re.compile('(\D+?)').findall(colStr)]
+    return reduceList([ord(c.upper())-65 for c in [idx for idx in idxs]])
+
+  def row(self,rowStr):
+    idxs = [match for match in re.compile('(\d+)').findall(rowStr)]
+    return reduceList([int(r)-1 for r in [idx for idx in idxs]])
+
+  def row_col(self,idxStr):
+    idxs = [match for match in re.compile('(\D+?)(\d+)').findall(idxStr)]
+    return reduceList([(int(r)-1,ord(c.upper())-65) for (c,r) in [idx for idx in idxs]])
+
+  def date(self,rawDate,datemode=0):
+    try:
+      date = xlrd.xldate.xldate_as_datetime(rawDate,datemode)
+    except:
+      date = dateparser.parse(rawDate)
+    return date
+
+class WorkSummaryForm(ExcelFile):
+
+  def __init__(self,filename):
+    ExcelFile.__init__(self,filename,idx['sheet'])
+    self.filename = filename
+    self.lut      = self.load_luts()
+    self.meta     = self.load_meta()
+    self.repairs  = self.load_repairs()
+
+  def load_luts(self):
+    return {
+      'result' : csv_to_list(lut['result']),
+      'fix'    : csv_to_list(lut['fix'])
+    }
+
+  def load_meta(self):
+    get = self.open().cell_value
+    return {
+      'year'     : str(self.date(get(*self.row_col(idx['date']))).year),
+      'country'  : str(          get(*self.row_col(idx['country']))),
+      'engineers': str(          get(*self.row_col(idx['engineers']))),
+      'hospital' : str(          get(*self.row_col(idx['hospital'])))
+    }
+
+  def load_repairs(self):
+    sheet = self.open()
+    get   = sheet.cell_value
+    repairs = []
+    for row in range(self.row(idx['repair']['row0']),sheet.nrows):
+      if get(row,self.col(idx['repair']['equip'])):
+        repairs.append(dict({
+          'equipment': str(get(row,self.col(idx['repair']['equip']))),
+          'oem'      : str(get(row,self.col(idx['repair']['oem']))),
+          'model'    : str(get(row,self.col(idx['repair']['model']))),
+          'sn'       : str(get(row,self.col(idx['repair']['sn']))),
+          'notes'    : str(get(row,self.col(idx['repair']['notes']))),
+          'fix'      : self.switch_to_str(
+                         [get(row,self.col(c)) for c in idx['repair']['fix']],
+                         self.lut['fix']),
+          'result'   : self.switch_to_str(
+                         [get(row,self.col(c)) for c in idx['repair']['result']],
+                         self.lut['result'])
+        },**self.meta))
+
+    return repairs
+
+  def switch_to_str(self,index_list,lut):
+    res = [s for (s,b) in zip(lut,index_list) if bool(b)]
+    if res:
+      return res[0]
+    else:
+      return None
+
+def csv_to_list(lutname):
+  lut = []
+  with open(lutname,'r') as csvfile:
+    for row in csv.reader(csvfile):
+      if len(row) == 1:
+        lut += [cell.lstrip() for cell in row]
+      else:
+        lut.append([cell.lstrip() for cell in row])
+  return lut
+
+def reduceList(L):
+  if len(L) == 0:
+    return []
+  elif len(L) == 1:
+    return L[0]
+  else:
+    return L
+
+def make_luts():
+  LUT  = {}
+  LUTA = {}
+  for (key,value) in lut.iteritems():
+    LUT[key]  = csv_to_list(value)
+    LUTA[key] = ['All']+LUT[key]
+  return LUT,LUTA
+
+def parse_all_work_summary_forms(root,outFile):
+  repairs = []
+  for (paths,dirs,files) in os.walk(root):
+    for f in files:
+      form = WorkSummaryForm(os.path.join(paths,f))
+      repairs += form.repairs
+  with open(outFile,'w') as csvOutFile:
+    writer = csv.writer(csvOutFile)
+    writer.writerow(repairs[0].keys())
+    for value in repairs:
+      writer.writerow(value.values())
+
+def load_all_repairs(inFile):
+  repairs = []
+  with open(inFile,'r') as csvInFile:
+    reader = csv.reader(csvInFile)
+    header = next(reader)
+    for row in reader:
+      repairs.append({key:str(value) for key,value in zip(header,row)})
+  return repairs
+
+def make_filters(year_idx,country_idx,equip_idx):
+  return {'year':     LUTA['year'][year_idx],
+          'country':  LUTA['country'][country_idx],
+          'equipment':LUTA['equipment'][equip_idx]}
+
+def filter_dict_list_by_dict(dictList,dictFilter,asType=str):
+  return [d for d in dictList if
+          all((asType(d[key]) in value) or (value in [None,'*','All','all'])
+          for key,value in dictFilter.iteritems())]
+
+def count_dict_list_value(dictList,key,values,asType=str):
+  return [[asType(d[key]) for d in dictList].count(value) for value in values]
+
+def remove_zeros(values,labels):
+  if values.count(0) is not len(values):
+    return zip(*((v,l) for v,l in zip(values,labels) if v is not 0))
+  else:
+    return [],[]
 
 # Initializations & Data -------------------------------------------------------
+#parse_all_work_summary_forms(wsf['root'],wsf['output']) # need only once
 app = dash.Dash(__name__)
 app.css.append_css({'external_url':'static/custom.css'})
 server = app.server
-# lookup tables
-LUT  = {}
-LUTA = {}
-for (key,value) in lut.iteritems():
-  LUT[key]  = csv_LUT(value)
-  LUTA[key] = ['All']+LUT[key]
-# get all repair entries from forms
-forms = get_all_work_summary_forms(wsf['root'])
-repairs = get_attr_from_obj_list(forms,'repairs',merge=True)
+LUT,LUTA = make_luts()
+repairs = load_all_repairs(wsf['output'])
 
 # Misc Functions ---------------------------------------------------------------
 
@@ -40,8 +221,17 @@ def make_dropdown(lutName,dropdownID):
       id=dropdownID,
       options=[{'label': [item], 'value': i} for (i,item) in enumerate(LUTA[lutName])],
       value=0)
-    ],
-    className='dropdown-container'
+    ],className='dropdown-container'
+  )
+
+def make_text_output(title,textid):
+  return html.Div([
+    html.Div([title+':'],style={'padding':'10px 10px 5px 10px'}),
+      html.Div([
+        html.Div(['51'],id=textid,style={'padding':'7px 10px 7px 10px'},
+        className='Select-control output'),
+      ],className='Select'),
+    ],className='dropdown-container',style={'vertical-align':'bottom'},
   )
 
 def make_pie(labels,values):
@@ -55,11 +245,13 @@ def make_pie(labels,values):
     'layout': go.Layout(
       margin={'l': 0, 'b': 120, 't': 0, 'r': 0, 'pad': 0},
       legend={'orientation':'h'},
-      autosize=True
+      autosize=True,
     )
   }
 
-def make_hbar(labels,values):
+def make_hbar(labels,values,nonZero=True):
+  if nonZero:
+    values,labels=remove_zeros(values,labels)
   return {
     'data': [go.Bar(
       x=values,
@@ -92,6 +284,7 @@ app.layout = html.Div([
       make_dropdown('year','select-year'),
       make_dropdown('country','select-country'),
       make_dropdown('equipment','select-equipment'),
+      make_text_output('Matching Repairs','match-count')
     ],className='panel-inner')
   ],className='panel-outer full-width'),
   html.Div([
@@ -123,10 +316,18 @@ app.layout = html.Div([
                style={'display':'inline-block','margin':'10px 0px 10px 0px'})
     ],className='panel-inner')
   ],className='panel-outer full-width'),
-], style={'width':'80%','margin':'0% 8% 8% 8%'}
-)
+],style={'width':'80%','margin':'0% 8% 8% 8%'})
 
 # Callback Functions -----------------------------------------------------------
+
+@app.callback(
+  Output('match-count','children'),
+ [Input('select-year','value'),
+  Input('select-country','value'),
+  Input('select-equipment','value')])
+def update_total_repairs(year_idx,country_idx,equip_idx):
+  return len(filter_dict_list_by_dict(
+    repairs,make_filters(year_idx,country_idx,equip_idx),asType=str))
 
 @app.callback(
   Output('pie-result','figure'),
@@ -134,11 +335,9 @@ app.layout = html.Div([
   Input('select-country','value'),
   Input('select-equipment','value')])
 def update_pie_result(year_idx,country_idx,equip_idx):
-  filters = {'year':     LUTA['year'][year_idx],
-             'country':  LUTA['country'][country_idx],
-             'equipment':LUTA['equipment'][equip_idx]}
-  repairs_filtered = filter_obj_list_multi(repairs,filters,asType=str)
-  results = count_obj_list_by_attr(repairs_filtered,'result',LUT['result'],asType=str)
+  repairs_filtered = filter_dict_list_by_dict(
+    repairs,make_filters(year_idx,country_idx,equip_idx),asType=str)
+  results = count_dict_list_value(repairs_filtered,'result',LUT['result'],asType=str)
   return make_pie(LUT['result'],results)
 
 @app.callback(
@@ -147,11 +346,9 @@ def update_pie_result(year_idx,country_idx,equip_idx):
   Input('select-country','value'),
   Input('select-equipment','value')])
 def update_pie_fix(year_idx,country_idx,equip_idx):
-  filters = {'year':     LUTA['year'][year_idx],
-             'country':  LUTA['country'][country_idx],
-             'equipment':LUTA['equipment'][equip_idx]}
-  repairs_filtered = filter_obj_list_multi(repairs,filters,asType=str)
-  results = count_obj_list_by_attr(repairs_filtered,'fix',LUT['fix'],asType=str)
+  repairs_filtered = filter_dict_list_by_dict(
+    repairs,make_filters(year_idx,country_idx,equip_idx),asType=str)
+  results = count_dict_list_value(repairs_filtered,'fix',LUT['fix'],asType=str)
   return make_pie(LUT['fix'],results)
 
 @app.callback(
@@ -160,18 +357,10 @@ def update_pie_fix(year_idx,country_idx,equip_idx):
   Input('select-country','value'),
   Input('select-equipment','value')])
 def update_bar(year_idx,country_idx,equip_idx):
-  filters = {'year':     LUTA['year'][year_idx],
-             'country':  LUTA['country'][country_idx],
-             'equipment':LUTA['equipment'][equip_idx]}
-  repairs_filtered = filter_obj_list_multi(repairs,filters,asType=str)
-  if repairs_filtered:
-    bar_year = count_obj_list_by_attr(repairs_filtered,'equipment',LUT['equipment'])
-    bar_year_nonzero,equipment_nonzero = \
-      zip(*((b,e) for b,e in zip(bar_year,LUT['equipment']) if b is not 0))
-  else:
-    equipment_nonzero = []
-    bar_year_nonzero = []
-  return make_hbar(equipment_nonzero,bar_year_nonzero)
+  repairs_filtered = filter_dict_list_by_dict(
+    repairs,make_filters(year_idx,country_idx,equip_idx),asType=str)
+  bar = count_dict_list_value(repairs_filtered,'equipment',LUT['equipment'])
+  return make_hbar(LUT['equipment'],bar)
 
 @app.callback(
   Output('repair-table','children'),
@@ -180,18 +369,14 @@ def update_bar(year_idx,country_idx,equip_idx):
   Input('select-equipment','value'),
   Input('table-toggles','values')])
 def update_notes(year_idx,country_idx,equip_idx,table_cols):
-  # print table_cols
   filters = {'year':     LUTA['year'][year_idx],
              'country':  LUTA['country'][country_idx],
              'equipment':LUTA['equipment'][equip_idx]}
-  repairs_filtered = filter_obj_list_multi(repairs,filters,asType=str)
-  tablecols = OrderedDict([row for row in LUT['table'] if row[1] in table_cols])
-  table     = make_table_row(tablecols.keys(),header=True)
-  colData   = OrderedDict()
-  for colAttr in tablecols.values():
-    colData[colAttr] = get_attr_from_obj_list(repairs_filtered,colAttr)
-  for rowData in zip(*colData.itervalues()):
-    table += make_table_row(list(rowData))
+  repairs_filtered = filter_dict_list_by_dict(repairs,filters,asType=str)
+  header,keys = zip(*[row for row in LUT['table'] if row[1] in table_cols])
+  table = make_table_row(header,header=True)
+  for row in zip(*[[repair[key] for repair in repairs_filtered] for key in keys]):
+    table += make_table_row(list(row))
   return table
 
 if __name__ == '__main__':
