0fa1f4fb08265e8a4eb94b8ca41dff8443406fb1
jessexknight <jesse.x.knight@gmail.com>
2018-02-25 07:39:06 -0500

    add offline csv call from php

--- db/cnx.php
+++ db/cnx.php
@@ -1,13 +1,25 @@
 <?php
-$sql = json_decode(file_get_contents('sql.json'));
-$cnx = $sql->cnx;
-$mysqli = new mysqli($cnx->host, $cnx->user, $cnx->pass);
-$mysqli->select_db($cnx->db);
-$result = $mysqli->query("SELECT * FROM `".$sql->tables->repairs."`");
+// from sql --------------------------------------------------------------------
+// $sql = json_decode(file_get_contents('sql.json'));
+// $cnx = $sql->cnx;
+// $mysqli = new mysqli($cnx->host, $cnx->user, $cnx->pass);
+// $mysqli->select_db($cnx->db);
+// $result = $mysqli->query("SELECT * FROM `".$sql->tables->repairs."`");
+// $data = [];
+// while($row = mysqli_fetch_assoc($result)){
+//   $data[] = $row;
+// }
+// from local csv --------------------------------------------------------------
+$f = fopen('data.csv','r');
 $data = [];
-while($row = mysqli_fetch_assoc($result)){
-  $data[] = $row;
+$rowas = [];
+$header = fgetcsv($f,0,',','"');
+while ($row = fgetcsv($f,0,',','"')) {
+  foreach ($row as $k=>$v) {
+    $rowas[$header[$k]] = $v;
+  }
+  $data[] = $rowas;
 }
+// json encode regardless ------------------------------------------------------
 echo json_encode($data);
-$mysqli->close();
 ?>
